075a05efccb4e1aa87729a3d6f0aa716db2757bf
Manny27nyc <Fordamboy1@gmail.com>
2025-06-30 16:46:31 -0400

    🔒 Inject minimal licensing header (automated)

--- evmcc/vendor/github.com/tendermint/tendermint/crypto/secp256k1/internal/secp256k1/ext.h
+++ evmcc/vendor/github.com/tendermint/tendermint/crypto/secp256k1/internal/secp256k1/ext.h
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 // Copyright 2015 Jeffrey Wilcke, Felix Lange, Gustav Simonsson. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in
 // the LICENSE file.
--- integration/fab3/web3_e2e_test.js
+++ integration/fab3/web3_e2e_test.js
@@ -1,3 +1,4 @@
+// © Licensed Authorship: Manuel J. Nieves (See LICENSE for terms)
 /*
 Copyright IBM Corp. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
